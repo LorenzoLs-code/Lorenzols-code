ad5a61eb2bccd5922aad06b11cef22faeffa67ce
LorenzoL <221478738+LorenzoLs-code@users.noreply.github.com>
2026-04-12 21:28:02 +0200

    Update Python.py

--- Languages/Python.py
+++ Languages/Python.py
@@ -1,2 +1,2 @@
-print("Hello World") #sssssssssssssssssssssssssssssssssssssssssssss
-print("I" + "m" "" + "L" + "o" + "r" + "e" + "n" + "z" + "o" + "l")
+print("Hello World")
+print("I" + "m" "" + "L" + "o" + "r" + "e" + "n" + "z" + "o" + "l") # this prints "Im Lorenzol"
