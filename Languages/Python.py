b762e2ba72979b872d74736cd4dd1f42aa38183b
LorenzoL <221478738+LorenzoLs-code@users.noreply.github.com>
2026-04-12 21:28:30 +0200

    Fix comment for print statement in Python.py

--- Languages/Python.py
+++ Languages/Python.py
@@ -1,2 +1,2 @@
 print("Hello World")
-print("I" + "m" "" + "L" + "o" + "r" + "e" + "n" + "z" + "o" + "l") # this prints "Im Lorenzol"
+print("I" + "m" "" + "L" + "o" + "r" + "e" + "n" + "z" + "o" + "l") # prints "Im Lorenzol"
